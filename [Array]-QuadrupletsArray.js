94a2463c1b4cea47fa6158f48e40bd0a8e221f2c
geraldgsh <geraldgsh@hotmail.com>
2020-06-28 15:07:11 +0800

    findArrayQuadruplet

---
+++ [Array]-QuadrupletsArray.js
@@ -0,0 +1,31 @@
+
+var twoSum = function (array, start_idx, s) {
+  let hash = {}
+  for (let i = start_idx; i < array.length; i++) {
+    let curr = array[i]
+    let sub = s - curr;
+    const check = hash[sub]
+    if (check !== undefined) {
+      return [sub, curr]
+    } else {
+      hash[curr] = i
+    }
+  }
+  return 0
+}
+
+function findArrayQuadruplet(arr, s) {
+  // your code goes here
+  arr = arr.sort((a,b) => a - b)
+  if (arr.length <= 3) return []
+  for (let i=0; i < arr.length - 3; i++) {
+    for (let j=i+1; j < arr.length - 2; j++) {
+      //let addition = twoSum(arr.slice(j+1, arr.length), s - (arr[i] + arr[j]))
+      let addition = twoSum(arr, j+1, s - (arr[i] + arr[j]))
+      if (addition) {
+        return [arr[i], arr[j]].concat(addition)//.sort((a,b) => a - b)   
+      }
+    }
+  }
+  return []
+}
